2f395e2f4e6abad8bb07701222f0958d8edc3e1a
Акрам <115643985+akramblch@users.noreply.github.com>
2022-10-14 17:27:34 +0600

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,30 @@
+const btnred = document.querySelector('.btnred')
+const btngreen = document.querySelector('.btngreen')
+const btnyellow = document.querySelector('.btnyellow')
+const btnreset = document.querySelector('.btnreset')
+
+const body = document.body
+
+btnred.addEventListener('click', () => {
+    let timer = setTimeout (() => {
+        body.style.backgroundColor = "red"
+        console.log("RED");
+    },3000)
+})
+btngreen.addEventListener('click', () => {
+    let timer2 = setTimeout (() => {
+        body.style.backgroundColor = "green"
+        console.log("GREEN");
+    },3000)
+})
+btnyellow.addEventListener('click', () => {
+    let timer3 = setTimeout (() => {
+        body.style.backgroundColor = "yellow"
+        console.log("YELLOW");
+    },3000)
+})
+btnreset.addEventListener('click', () => {
+    let reset = setTimeout(() => {
+        body.style.backgroundColor = '#fff'
+    })
+})
